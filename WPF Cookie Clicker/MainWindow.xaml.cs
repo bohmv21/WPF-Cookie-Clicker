32fd2f52842e73961c9834f47b3ed1e2ada247e7
Bo Loomans <bo.loomans204947@student.leeuwenborgh.nl>
2017-12-07 17:34:53 +0100

    df

--- WPF Cookie Clicker/MainWindow.xaml.cs
+++ WPF Cookie Clicker/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace WPF_Cookie_Clicker
 {
@@ -20,9 +21,82 @@ namespace WPF_Cookie_Clicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double CookiesPerClick = 1;
+        public double CookiesPerSecond;
+        public double TotalCookies;
+
+        public double ClickPrice = 15;
+        public double GrandmaPrice = 100;
         public MainWindow()
         {
             InitializeComponent();
+            Thread thread = new Thread(Cookies);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        private void Cookies()
+        {
+            while (true)
+            {
+
+                Thread.Sleep(100);
+
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    TotalCookies = TotalCookies + CookiesPerSecond/10;
+                    txtCookiesPs.Text = Convert.ToString(CookiesPerSecond);
+                    txtTotalCookies.Text = Convert.ToString(Convert.ToInt32(TotalCookies));
+
+                    if (TotalCookies >= ClickPrice)
+                    {
+                        btnClick.IsEnabled = true;
+                    } else
+                    {
+                        btnClick.IsEnabled = false;
+                    }
+
+                    if (TotalCookies >= GrandmaPrice)
+                    {
+                        btnGrandma.IsEnabled = true;
+                    }
+                    else
+                    {
+                        btnGrandma.IsEnabled = false;
+                    }
+
+                }));
+            }
+        }
+
+        private void Cookie_Click(object sender, RoutedEventArgs e)
+        {
+            TotalCookies = TotalCookies + CookiesPerClick;
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            if (TotalCookies >= ClickPrice)
+            {
+                CookiesPerSecond = CookiesPerSecond + 0.1;
+                TotalCookies = TotalCookies - ClickPrice;
+                double Price = ClickPrice * 20 / 100;
+                ClickPrice = ClickPrice + Price;
+                txtCLick.Text = Convert.ToString(Convert.ToInt32(ClickPrice));
+            }
+        }
+
+        private void btnGrandma_Click(object sender, RoutedEventArgs e)
+        {
+            if (TotalCookies >= GrandmaPrice)
+            {
+                CookiesPerSecond = CookiesPerSecond + 1;
+                TotalCookies = TotalCookies - GrandmaPrice;
+                double Price = GrandmaPrice * 15 / 100;
+                GrandmaPrice = GrandmaPrice + Price;
+                txtGrandma.Text = Convert.ToString(Convert.ToInt32(GrandmaPrice));
+            }
         }
     }
 }
